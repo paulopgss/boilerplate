958bd7cafb2a42a7753b7de032fea092620aaa08
Paulo Sousa <paulopgss@gmail.com>
2022-01-30 15:30:37 -0300

    feat: :sparkles: plop
    add plop

---
+++ generators/plopfile.js
@@ -0,0 +1,20 @@
+export default function (plop) {
+  // controller generator
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      }
+    ]
+  })
+}
